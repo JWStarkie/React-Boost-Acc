d9925a00b97927c5f619fadd1a72b5ab0d0ca24f
JWStarkie <jstarkie@live.co.uk>
2019-03-12 21:37:39 +0000

    Removing node_modules folder.

--- webpack.config.js
+++ webpack.config.js
@@ -26,4 +26,15 @@ module.exports = {
         contentBase: './dist',
         hot: true
     }
+}; var webpack = require('webpack');
+var path = require('path');
+var BUILD_DIR = path.resolve(__dirname, 'src/frontend/public');
+var APP_DIR = path.resolve(__dirname, 'src/frontend/app');
+var config = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    }
 };
+module.exports = config;
